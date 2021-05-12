38e8b45dce0d27a61fca6a8cf16bc01ecee3d5c2
Mouli <venkatmouli823@gmail.com>
2021-05-12 20:53:01 +0530

    viewall hotels

--- foodHospitality/hotels/views.py
+++ foodHospitality/hotels/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Hotel
 # Create your views here.
 
 
 def AllHotels(request):
-    return HttpResponse('<p> All hotels</p>')
+    hotels = Hotel.objects.all()
+    return render(request,'hotels/viewhotels.html',{
+        'hotels' : hotels
+    })
 
 def FetchHotel(request,id):
     return HttpResponse('<p> Fetch single hotel {{id}} </p>')
