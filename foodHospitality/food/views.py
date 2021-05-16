e2c3323abce0c0f6ded32a8cbd97258b73b20f6a
Mouli <venkatmouli823@gmail.com>
2021-05-16 20:01:54 +0530

    Changes

--- foodHospitality/food/views.py
+++ foodHospitality/food/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Category,Food,Cart
 from django.contrib.auth.models import User
 from .forms import OrderForm
+
  # Create your views here.
 
 def Menu(request):
