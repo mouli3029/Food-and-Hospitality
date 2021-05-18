4ae1c6da17954b498ef8a1684e81d797d373cac4
Mouli <venkatmouli823@gmail.com>
2021-05-18 15:14:13 +0530

    ADD : Updating bookings

--- foodHospitality/hotels/views.py
+++ foodHospitality/hotels/views.py
@@ -88,9 +88,61 @@ def YourBookings(request):
         'hotel__name', # Similar to booking.hotel.value
         'hotel__price',
         'hotel__image',
+        'id',
     )
     return render(request,'hotels/allbookings.html',{
         'bookings' : bookings
+    })
+
+def UpdateBooking(request,id):
+    booking_details = get_object_or_404(Booking,pk=id)
+    hotel_id = booking_details.hotel.id
+    user_id = booking_details.user.id
+
+    # Getting user and hotel based on booking
+    hotel = get_object_or_404(Hotel,pk=hotel_id)
+    user  = get_object_or_404(User,pk=user_id)
+
+    
+    # Post Method
+    if request.method == "POST":
+        filled_form = BookingForm(request.POST)
+        print(filled_form)
+        if filled_form.is_valid():
+            
+            quantity = filled_form.cleaned_data['quantity']
+            booked_from = filled_form.cleaned_data['booked_from']
+            booked_to = filled_form.cleaned_data['booked_to']
+
+            book_detail = Booking(id=id,user=user,hotel=hotel,quantity=quantity,booked_from=booked_from,booked_to=booked_to)
+            book_detail.save()
+
+            mess = {
+                "title" : "Updated Successfully!Please check in your bookings",
+                "ordername" : hotel.name
+            }
+        else :
+            mess = {
+                "title" : "Something went wrong.We are working on it. Please try after few minutes !" ,
+                "ordername": ""
+                }
+        return render(request,'hotels/booking_update.html',{
+         'id' :id,
+         "mess":mess
+    })
+    
+    else :
+        form = BookingForm()
+        form.fields['email'].initial = user.email
+        form.fields['username'].initial = user.username
+        form.fields['name'].initial = hotel.name 
+        form.fields['quantity'].initial = booking_details.quantity
+        form.fields['booked_to'].initial = booking_details.booked_to
+
+    print(booking_details)
+    return render(request,'hotels/booking_update.html',{
+        'form' : form,
+        "id":id
     })
     
 
